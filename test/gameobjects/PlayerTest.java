8707215cf98a635bfdb7d1477989d4e09f4d0883
Aaron Buchholz <bucaar@hotmail.com>
2017-10-19 21:33:33 -0500

    Added test for ship location, validate out of bounds, overlapping ships

--- test/gameobjects/PlayerTest.java
+++ test/gameobjects/PlayerTest.java
@@ -8,21 +8,22 @@ package gameobjects;
 import java.io.File;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author aaron
  */
 public class PlayerTest {
+    private BattleShipPlayer player;
     
     public PlayerTest() {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    @Before
+    public void setupClass(){
+        player = getPlayerInstance();
+    }
     
     private Class<? extends BattleShipPlayer> getPlayerClass(){
         String packageName = BattleShipPlayer.class.getPackage().getName();
@@ -70,8 +71,74 @@ public class PlayerTest {
         return player;
     }
     
-    @Test
-    public void testPlayerClass(){
-        BattleShipPlayer player = getPlayerInstance();
+    @Test(timeout = 1000L)
+    public void testGetShipLayout(){
+        for(int iteration = 0; iteration < 10; iteration++){
+            String enemyLayout = player.getShipLayout().toString();
+            enemyLayout = enemyLayout.substring(1, enemyLayout.length()-1);
+            if(enemyLayout.length() == 0){
+                fail("ShipLayout is not placing any ships.");
+            }
+            String[][] board = new String[10][10];
+
+            String[] ships = enemyLayout.split("], ");
+            for(String ship : ships){
+                String id = ship.substring(1, 2);
+                int ioxs = ship.indexOf("[") + 1;
+                int ioxe = ship.indexOf(",");
+                int ioys = ioxe + 2;
+                int ioye = ship.lastIndexOf(",");
+                int ioo  = ship.lastIndexOf("\"") - 1;
+                int x = Integer.parseInt(ship.substring(ioxs,ioxe));
+                int y = Integer.parseInt(ship.substring(ioys,ioye));
+                String o = ship.substring(ioo, ioo+1);
+
+
+                int length=0;
+                switch(id){
+                    case "C": length = 5; break;
+                    case "B": length = 4; break;
+                    case "S":
+                    case "D": length = 3; break;
+                    case "P": length = 2; break;
+                    default:  fail("Invalid ship id placed: " + id); 
+                }
+
+                int width=1, height=1;
+                switch(o){
+                    case "h": width  *= length; break;
+                    case "v": height *= length; break;
+                    default:  fail("Invalid ship orientation " + o);
+                }
+
+                for(int bx=x;bx<x+width;bx++){
+                    for(int by=y;by<y+height;by++){
+                        try{
+                            if(board[bx][by] != null){
+                                fail("Placement of ship " + id + " overlaps with ship " + board[bx][by]);
+                            }
+                            board[bx][by] = id;
+                        }
+                        catch(IndexOutOfBoundsException e){
+                            fail("Ship " + id + " was placed out of bounds at " + bx + ", " + by);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @Test(timeout = 5000L)
+    public void testGetShotLocation(){
+        int[][][] myShipCoords = new int[][][]{
+            {{0,0},{1,0},{2,0},{3,0},{4,0}},
+            {{0,1},{1,1},{2,1},{3,1}},
+            {{0,2},{1,2},{2,2}},
+            {{0,3},{1,3},{2,3}},
+            {{0,4},{1,4}}
+        };
+        int[] myShipHitCount = new int[5];
+        int myShotX = 0, myShotY = 0;
+        int[][] enemyShots = new int[100][2];
     }
 }
